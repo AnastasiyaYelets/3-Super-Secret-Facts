89463c4f665b670393f8b111ac67c3e5a4e39fc9
MsZlets <you@example.com>
2016-12-12 11:35:04 +0700

    First commit

---
+++ src/components/resources.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default () => {
+    return (
+      <div>
+        <div>
+      3 Super Special Secret Facts:
+      <ul>
+        <li>Unicorns exist.</li>
+        <li>Chocolate milk and Thai massage are good for your health.</li>
+        <li>Raccoons are funny and they are the best pets in the world.</li>
+      </ul>
+      </div>
+      <div>
+      1 Extra Super Special Secret Fact:
+      <ul>
+        <li>Kirill is the kind little raccoon.</li>
+      </ul>
+      </div>
+        </div>
+    );
+  }
